e614f0f29f7787a5aca8e4b6887925f5588ef050
lmk030 <32295001+lmk030@users.noreply.github.com>
2017-09-26 21:45:04 +0800

    001

---
+++ hello world/hello world/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hello world
+//
+//  Created by 李民凯 on 2017/9/26.
+//  Copyright © 2017年 llmklmk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
