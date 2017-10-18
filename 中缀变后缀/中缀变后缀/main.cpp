9f2a108864b6cbafa9bb5282eb6cc93f439cfe63
lmk030 <32295001+lmk030@users.noreply.github.com>
2017-10-18 16:07:02 +0800

    中缀变后缀

--- 中缀变后缀/中缀变后缀/main.cpp
+++ 中缀变后缀/中缀变后缀/main.cpp
@@ -1,20 +1,13 @@
-//
-//  main.cpp
-//  中缀变后缀
-//
-//  Created by 李民凯 on 2017/10/12.
-//  Copyright © 2017年 llmklmk. All rights reserved.
-//
+
 
 #include <iostream>
 #include <stack>
-#include <string>
-#include <cctype>
 
 using namespace std;
 int compare(char op1, char op2)
 {
-    if (op2 == '=') {
+    if (op2 == '=')
+    {
         return 1;
     }
     if (op1 == '(')
@@ -43,11 +36,11 @@ int compare(char op1, char op2)
             return 0;
         }
     }
-        return -1;
+    return -1;
 }
 
 int main(int argc, const char * argv[]) {
-
+    
     stack <char>s2;
     s2.push('=');
     string s;
@@ -61,15 +54,16 @@ int main(int argc, const char * argv[]) {
             {
                 j++;
             }
-            cout << s.substr(i,j - i);
+            cout << s.substr(i, (j - i));
         }
         else
         {
             if (s[i] != ')')
             {
-                if (compare(s[i], s2.top()) == 1)
+                if (1 == compare(s[i], s2.top()))
                 {
                     s2.push(s[i]);
+                    continue;
                 }
                 if (compare(s[i], s2.top()) == 0)
                 {
@@ -90,7 +84,7 @@ int main(int argc, const char * argv[]) {
                 }
                 s2.pop();
             }
-    
+            
         }
     }
     while (s2.top() != '=') {
